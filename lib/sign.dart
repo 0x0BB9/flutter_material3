b3609787a6d3d10e970e381be397f98993e0a732
will.ai <airbknows@gmail.com>
2023-10-20 17:04:31 +0800

    [update] plain style

--- lib/sign.dart
+++ lib/sign.dart
@@ -10,12 +10,6 @@ class SignPage extends StatefulWidget {
 const TEXT_NORMAL_SIZE = 14.0;
 const TEXT_LARGE_SIZE = 22.0;
 
-const MAIN_COLOR = Color(0xFF303030);
-const YELLOW = Color(0xfffbed96);
-const BLUE = Color(0xffabecd6);
-const SIGNUP_LIGHT_RED = Color(0xffffc2a1);
-const SIGNUP_RED = Color(0xffffb1bb);
-
 class _SignPageState extends State<SignPage> {
   final TextEditingController _username = TextEditingController();
   final TextEditingController _password = TextEditingController();
@@ -28,13 +22,6 @@ class _SignPageState extends State<SignPage> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: FractionalOffset(0.0, 0.4), end: FractionalOffset(0.9, 0.7),
-            // Add one stop for each color. Stops should increase from 0 to 1
-            stops: [0.1, 0.9], colors: [YELLOW, BLUE],
-          ),
-        ),
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
           child: Stack(
@@ -85,39 +72,23 @@ class _SignPageState extends State<SignPage> {
                         ),
                         Container(
                           height: _media.height / 3.8,
-                          decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              tileMode: TileMode.clamp,
-                              begin: FractionalOffset.centerLeft,
-                              end: FractionalOffset.centerRight,
-                              stops: [0.1, 1.0],
-                              colors: [SIGNUP_LIGHT_RED, SIGNUP_RED],
-                            ),
-                            borderRadius: BorderRadius.circular(15),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 15,
-                                spreadRadius: 8,
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(30.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: inputText("USERNAME",
+                                        'will@gmail.com', _username, false),
+                                  ),
+                                  const Divider(
+                                    height: 5,
+                                  ),
+                                  Expanded(
+                                      child: inputText(
+                                          "PASSWORD", '******', _password, true)),
+                                ],
                               ),
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(30.0),
-                            child: Column(
-                              children: <Widget>[
-                                Expanded(
-                                  child: inputText("USERNAME",
-                                      'hristov123@gmail.com', _username, false),
-                                ),
-                                const Divider(
-                                  height: 5,
-                                  color: Colors.black,
-                                ),
-                                Expanded(
-                                    child: inputText(
-                                        "PASSWORD", '******', _password, true)),
-                              ],
                             ),
                           ),
                         ),
@@ -130,7 +101,6 @@ class _SignPageState extends State<SignPage> {
                     children: <Widget>[
                       const Text(
                         "Don't have an account?",
-                        style: TextStyle(color: MAIN_COLOR),
                       ),
                       const SizedBox(
                         width: 5,
@@ -150,8 +120,8 @@ class _SignPageState extends State<SignPage> {
                 bottom: _media.height / 6.3,
                 right: 15,
                 child: buildSignUpArrowButton(
-                  icon: const IconData(0xe901, fontFamily: 'Icons'),
-                  iconSize: 9,
+                  icon: Icons.arrow_forward,
+                  iconSize: 19,
                   onTap: () => debugPrint("Signup Tapped"),
                 ),
               ),
@@ -168,38 +138,15 @@ class _SignPageState extends State<SignPage> {
     VoidCallback? onTap,
     double height = 50.0,
     double width = 50.0,
-    Color iconColor = const Color(0xFFdbedb0),
   }) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
+    return FloatingActionButton(
+      onPressed: onTap,
+      child: SizedBox(
         height: height,
         width: width,
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black12,
-                blurRadius: 15,
-                spreadRadius: 0,
-                offset: Offset(0.0, 16.0)),
-          ],
-          gradient: LinearGradient(
-              begin: FractionalOffset.centerLeft,
-// Add one stop for each color. Stops should increase from 0 to 1
-              stops: [
-                0.2,
-                1
-              ],
-              colors: [
-                Color(0xff000000),
-                Color(0xff434343),
-              ]),
-        ),
         child: Icon(
           icon,
           size: iconSize,
-          color: iconColor,
         ),
       ),
     );
