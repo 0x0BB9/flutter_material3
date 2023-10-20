b50ba6b27e862909363c457797a6fba1e6c7218a
will.ai <airbknows@gmail.com>
2023-10-20 14:30:15 +0800

    [add] sign page

---
+++ lib/sign.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+
+class SignPage extends StatefulWidget {
+
+  @override
+  _SignPageState createState() {
+    return _SignPageState();
+  }
+}
+const TEXT_NORMAL_SIZE = 14.0;
+const TEXT_LARGE_SIZE = 22.0;
+
+const MAIN_COLOR = Color(0xFF303030);
+const YELLOW = Color(0xfffbed96);
+const BLUE = Color(0xffabecd6);
+const SIGNUP_LIGHT_RED = Color(0xffffc2a1);
+const SIGNUP_RED = Color(0xffffb1bb);
+
+class _SignPageState extends State<SignPage>{
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset(0.0, 0.4), end: FractionalOffset(0.9, 0.7),
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.9], colors: [YELLOW, BLUE],
+          ),
+        ),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 60.0, horizontal: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Center(
+                          child: Image.asset(
+                            "assets/images/will_logo.gif",
+                            height: _media.height / 7,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const Text(
+                          "WELCOME BACK!",
+                          style: TextStyle(
+                            letterSpacing: 4,
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.bold,
+                            fontSize: TEXT_LARGE_SIZE,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          'Log in',
+                          style: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w200,
+                              fontSize: 40),
+                        ),
+                        const Text(
+                          'to continue.',
+                          style: TextStyle(
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w200,
+                              fontSize: 40),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Container(
+                          height: _media.height / 3.8,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              tileMode: TileMode.clamp,
+                              begin: FractionalOffset.centerLeft,
+                              end: FractionalOffset.centerRight,
+                              stops: [0.1, 1.0],
+                              colors: [SIGNUP_LIGHT_RED, SIGNUP_RED],
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 15,
+                                spreadRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                  child: inputText("USERNAME",
+                                      'hristov123@gmail.com', _username, false),
+                                ),
+                                const Divider(
+                                  height: 5,
+                                  color: Colors.black,
+                                ),
+                                Expanded(
+                                    child: inputText(
+                                        "PASSWORD", '******', _password, true)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: MAIN_COLOR),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        onTap: () => debugPrint("Sign Up Tapped"),
+                        child: const Text("Sing Up"),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: _media.height / 6.3,
+                right: 15,
+                child: buildSignUpArrowButton(
+                  icon: const IconData(0xe901, fontFamily: 'Icons'),
+                  iconSize: 9,
+                  onTap: () => debugPrint("Signup Tapped"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignUpArrowButton({
+    IconData? icon,
+    double? iconSize,
+    VoidCallback? onTap,
+    double height = 50.0,
+    double width = 50.0,
+    Color iconColor = const Color(0xFFdbedb0),
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: height,
+        width: width,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                blurRadius: 15,
+                spreadRadius: 0,
+                offset: Offset(0.0, 16.0)),
+          ],
+          gradient: LinearGradient(begin: FractionalOffset.centerLeft,
+// Add one stop for each color. Stops should increase from 0 to 1
+              stops: [
+                0.2,
+                1
+              ], colors: [
+                Color(0xff000000),
+                Color(0xff434343),
+              ]),
+        ),
+        child: Icon(
+          icon,
+          size: iconSize,
+          color: iconColor,
+        ),
+      ),
+    );
+  }
+
+  Widget inputText(
+      String fieldName,
+      String hintText,
+      TextEditingController controller,
+      bool obSecure,
+      ) {
+    return TextField(
+      style: const TextStyle(height: 1.3),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: fieldName,
+        labelStyle: const TextStyle(
+          fontSize: TEXT_NORMAL_SIZE,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w400,
+          letterSpacing: 1,
+          height: 0,
+        ),
+        border: InputBorder.none,
+      ),
+      obscureText: obSecure,
+    );
+  }
+}
+
